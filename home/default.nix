19a43b55faf1b8ec72764235a7530bf78843d059
tobyck <toby_ck@icloud.com>
2024-08-12 12:51:44 +1200

    Add Cava

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
 
 		./programs/nvim
 		./programs/firefox.nix
+		./programs/cava.nix
 
 		./services/kanata
 
---
+++ home/programs/cava.nix
@@ -0,0 +1,18 @@
+{
+	programs.cava = {
+		enable = true;
+		settings = {
+			color = {
+				gradient = 1;
+				gradient_color_1 = "'#7aa2f7'";
+				gradient_color_2 = "'#7dcfff'";
+				gradient_color_3 = "'#bb9af7'";
+				gradient_color_4 = "'#9ece6a'";
+				gradient_color_5 = "'#e0af68'";
+				gradient_color_6 = "'#ff9e64'";
+				gradient_color_7 = "'#f7768e'";
+				gradient_color_8 = "'#db4b4b'";
+			};
+		};
+	};
+}
