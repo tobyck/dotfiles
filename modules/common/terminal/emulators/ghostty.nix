cf13937a4bbe1ac699a28307550664b7c2ef45e1
Toby Connor-Kebbell <toby_ck@icloud.com>
2025-08-05 10:55:34 +1200

    forgot i was making ghostty get theme from gh

--- modules/common/terminal/emulators/ghostty.nix
+++ modules/common/terminal/emulators/ghostty.nix
@@ -1,18 +1,27 @@
 { pkgs, ... }:
 
 {
-	programs.ghostty = {
-		enable = true;
-		package = null;
+  programs.ghostty =
+    let
+      vague-theme = pkgs.fetchFromGitHub {
+        owner = "vague2k";
+        repo = "vague.nvim";
+        rev = "676e5b4d74f32ff91c1583eba9c326d2b749672f";
+        hash = "sha256-rJe0pJu6/JKCCdhKTwPFdHlZ2CUaIJc++nlsEdYXQOY=";
+      };
+    in
+    {
+      enable = true;
+      package = null;
 
-		settings = {
-			theme = "/Users/toby/.config/dotfiles/modules/common/terminal/emulators/vague.txt";
-			command = "${pkgs.fish}/bin/fish --login --interactive";
-			shell-integration = "fish";
-			font-family = "Iosevka NFM";
-			font-size = 13.5;
-			window-padding-x = 6;
-			window-padding-y = 4;
-		};
-	};
+      settings = {
+        theme = "${vague-theme}/extras/ghostty/vague";
+        command = "${pkgs.fish}/bin/fish --login --interactive";
+        shell-integration = "fish";
+        font-family = "Iosevka NFM";
+        font-size = 13.5;
+        window-padding-x = 6;
+        window-padding-y = 4;
+      };
+    };
 }
